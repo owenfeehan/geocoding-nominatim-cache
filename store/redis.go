a185631cb6e8d16634b148ea17da8923be5dbb55
Owen Feehan <owenfeehan@users.noreply.github.com>
2025-06-21 15:20:04 +0200

    Rename receiver variable in redis.go to match type-name

--- store/redis.go
+++ store/redis.go
@@ -38,12 +38,12 @@ func NewRedisStore(address string) LocationStore {
 	return &redisStore{redis: client}
 }
 
-func (c *redisStore) BuildKey(query string) string {
+func (rs *redisStore) BuildKey(query string) string {
 	return fmt.Sprintf("geocode:%s", strings.ToLower(query))
 }
 
-func (c *redisStore) Get(key string) ([]location.Location, error) {
-	cached, err := c.redis.Get(ctx, key).Result()
+func (rs *redisStore) Get(key string) ([]location.Location, error) {
+	cached, err := rs.redis.Get(ctx, key).Result()
 	if err == redis.Nil {
 		return nil, nil
 	} else if err != nil {
@@ -56,16 +56,16 @@ func (c *redisStore) Get(key string) ([]location.Location, error) {
 	return result, nil
 }
 
-func (c *redisStore) Set(key string, locations []location.Location) error {
+func (rs *redisStore) Set(key string, locations []location.Location) error {
 	body, err := marshalLocations(locations)
 	if err != nil {
 		return err
 	}
-	return c.redis.Set(ctx, key, body, 0).Err()
+	return rs.redis.Set(ctx, key, body, 0).Err()
 }
 
-func (c *redisStore) Close() error {
-	return c.redis.Close()
+func (rs *redisStore) Close() error {
+	return rs.redis.Close()
 }
 
 // Assert implementation
